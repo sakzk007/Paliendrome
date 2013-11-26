cf55dfc797dc21398ce343154e384d4895ce28e0
sakzk007 <sakzk007@gmail.com>
2013-11-26 23:11:31 +0530

    changed coding format according to java convention.

--- src/com/anees/paliiendrome/AlertDialogManager.java
+++ src/com/anees/paliiendrome/AlertDialogManager.java
@@ -5,19 +5,18 @@ import android.content.Context;
 import android.content.DialogInterface;
 
 public class AlertDialogManager {
-	@SuppressWarnings("deprecation")
-	public void showAlertDialog(final Context context, String title,
-			String message, Boolean status) {
-		AlertDialog alertDialog = new AlertDialog.Builder(context).create();
-		// alertDialog.setTitle(title);
-		alertDialog.setMessage(message);
-		// Setting OK Button
-		alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int which) {
-				dialog.dismiss();
-			}
-		});
-		// Showing Alert Message
-		alertDialog.show();
-	}
+    @SuppressWarnings("deprecation")
+    public void showAlertDialog(final Context context, String title, String message, Boolean status) {
+	AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+	// alertDialog.setTitle(title);
+	alertDialog.setMessage(message);
+	// Setting OK Button
+	alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+	    public void onClick(DialogInterface dialog, int which) {
+		dialog.dismiss();
+	    }
+	});
+	// Showing Alert Message
+	alertDialog.show();
+    }
 }
--- src/com/anees/paliiendrome/MainActivity.java
+++ src/com/anees/paliiendrome/MainActivity.java
@@ -11,63 +11,60 @@ import android.app.Activity;
 
 public class MainActivity extends Activity {
 
-	private Button btnCheck;
-	private EditText etText;
-	private CheckBox chkBox;
-	private AlertDialogManager alrt;
+    private Button btnCheck;
+    private EditText etText;
+    private CheckBox chkBox;
+    private AlertDialogManager alrt;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		initVars();
-		btnCheck.setOnClickListener(new View.OnClickListener() {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_main);
+	initVars();
+	btnCheck.setOnClickListener(new View.OnClickListener() {
 
-			@Override
-			public void onClick(View v) {
-				// Check whether the check-box is checked or not
-				Log.d("Clicked", "True");
-				if (chkBox.isChecked() == true) {
-					// Show alert
-					Log.d("Checked", "True");
-					showAlertByChecking();
-				}
-			}
+	    @Override
+	    public void onClick(View v) {
+		// Check whether the check-box is checked or not
+		Log.d("Clicked", "True");
+		if (chkBox.isChecked() == true) {
+		    // Show alert
+		    Log.d("Checked", "True");
+		    showAlertByChecking();
+		}
+	    }
 
-			private void showAlertByChecking() {
-				StringBuffer txt = new StringBuffer();
-				String t1 = etText.getText().toString().trim();
-				txt.append(t1);
-				// Checking entered Text is whether empty
-				if (t1.length() != 0 && t1 != null) {
-					Log.d("Paliendrome String", t1);
-					if (alrt == null) {
-						Log.d("Alert Dialog Object", "Empty");
-						alrt = new AlertDialogManager();
-					}
-					if (t1.equalsIgnoreCase(txt.reverse().toString())) {
-						Log.d("Paliendrome", "True-");
+	    private void showAlertByChecking() {
+		StringBuffer txt = new StringBuffer();
+		String t1 = etText.getText().toString().trim();
+		txt.append(t1);
+		// Checking entered Text is whether empty
+		if (t1.length() != 0 && t1 != null) {
+		    Log.d("Paliendrome String", t1);
+		    if (alrt == null) {
+			Log.d("Alert Dialog Object", "Empty");
+			alrt = new AlertDialogManager();
+		    }
+		    if (t1.equalsIgnoreCase(txt.reverse().toString())) {
+			Log.d("Paliendrome", "True-");
 
-						alrt.showAlertDialog(MainActivity.this, "checking", t1
-								+ " is Paliendrome", true);
-					} else {
-						alrt.showAlertDialog(MainActivity.this, "checking", t1
-								+ " is not Paliendrome", true);
-					}
+			alrt.showAlertDialog(MainActivity.this, "checking", t1 + " is Paliendrome", true);
+		    } else {
+			alrt.showAlertDialog(MainActivity.this, "checking", t1 + " is not Paliendrome", true);
+		    }
 
-				} else {
-					Toast.makeText(getApplicationContext(), "Field is Empty!",
-							Toast.LENGTH_SHORT).show();
-				}
+		} else {
+		    Toast.makeText(getApplicationContext(), "Field is Empty!", Toast.LENGTH_SHORT).show();
+		}
 
-			}
-		});
-	}
+	    }
+	});
+    }
 
-	private void initVars() {
-		btnCheck = (Button) findViewById(R.id.button1);
-		etText = (EditText) findViewById(R.id.editText1);
-		chkBox = (CheckBox) findViewById(R.id.checkBox1);
-	}
+    private void initVars() {
+	btnCheck = (Button) findViewById(R.id.button1);
+	etText = (EditText) findViewById(R.id.editText1);
+	chkBox = (CheckBox) findViewById(R.id.checkBox1);
+    }
 
 }
